f105baa7e88cd7ad79ccd65518e008598e2b2c8f
dhananjay harel <dj@programmr.com>
2020-02-14 23:16:46 +0530

    some changes

--- finddogbreed/.runfile.sh
+++ finddogbreed/.runfile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Processing the image please wait.."
-:> .testcaseop2 && python3 dogbreed.py &> .testcaseop2
+:> .testcaseop2 && python3 dogbreed.py > .testcaseop2
 
 if grep -q 'Error: ' .testcaseop2; then
    echo "SYNTAXERROR"
