a9b8bbd428f6f11a1b8fc61f2acddd9317e1dcfb
dhananjay <harel.dhananjay@gmail.com>
2020-02-17 16:50:26 +0530

    added new examples

---
+++ images_and_numpy/.runfile.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo "Processing the image please wait.."
+:> .testcaseop2 && python3 images_and_numpy.py > .testcaseop2
+
+if grep -q 'Error: ' .testcaseop2; then
+   echo "SYNTAXERROR"
+else if grep -q 'SUCCESS' .testcaseop2; then
+   echo "SUCCESS"
+else
+   echo "UNKNOWNERROR"    
+fi
+fi
+echo ""
+echo "!done!"
